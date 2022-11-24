8c2025d9b0ec6fb3c0321405dc17df5c1296e844
mahian ahmad <mahianahamd9@gmail.com>
2022-11-24 15:55:27 +0600

    connect with mongodb

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const product = require('./product.json');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-app.get("/", (req, res)=> {
+
+const uri = "mongodb+srv://mahian:ExiP934rnJVB4VIe@cluster0.zft8w2s.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  client.close();
+});
+
+
+app.get("/products", (req, res)=> {
     res.send(product);
 });
 
